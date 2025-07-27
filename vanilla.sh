76d9ec2e30bf138819c47a16edfe89d0f7e7fda0
南極雄 <mcg25035@gmail.com>
2025-07-28 03:55:44 +0800

    remove debug code

--- vanilla.sh
+++ vanilla.sh
@@ -4,18 +4,11 @@
 # Server Files: /mnt/server
 mkdir -p /mnt/server
 cd /mnt/server
-
-echo "test"
-
 LATEST_VERSION=`curl https://launchermeta.mojang.com/mc/game/version_manifest.json | jq -r '.latest.release'`
 LATEST_SNAPSHOT_VERSION=`curl https://launchermeta.mojang.com/mc/game/version_manifest.json | jq -r '.latest.snapshot'`
 
-echo "test2"
-
 echo -e "latest version is $LATEST_VERSION"
 echo -e "latest snapshot is $LATEST_SNAPSHOT_VERSION"
-
-echo "test3"
 
 if [ -z "$MC_VERSION" ] || [ "$MC_VERSION" == "latest" ]; then
   MANIFEST_URL=$(curl -sSL https://launchermeta.mojang.com/mc/game/version_manifest.json | jq --arg VERSION $LATEST_VERSION -r '.versions | .[] | select(.id== $VERSION )|.url')
@@ -25,16 +18,9 @@ else
   MANIFEST_URL=$(curl -sSL https://launchermeta.mojang.com/mc/game/version_manifest.json | jq --arg VERSION $MC_VERSION -r '.versions | .[] | select(.id== $VERSION )|.url')
 fi
 
-echo "test4"
-
 DOWNLOAD_URL=$(curl ${MANIFEST_URL} | jq .downloads.server | jq -r '. | .url')
-
-echo "test5"
 
 echo -e "running: curl -o ${SERVER_JARFILE} $DOWNLOAD_URL"
 curl -o ${SERVER_JARFILE} $DOWNLOAD_URL
-
-echo "test6"
-
 
 echo -e "Install Complete"
